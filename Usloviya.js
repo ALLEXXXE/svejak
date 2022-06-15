bd25e3ec62c50a87e3311ba6dba3a9f7092e33ab
Alexey <allexxxe@inbox.ru>
2022-06-15 22:31:16 +0200

    Условия

---
+++ Usloviya.js
@@ -0,0 +1,37 @@
+"use strict";
+
+if (4 == 4) {
+    console.log('Ok!');
+}
+else {
+    console.log('Error');
+}
+
+const num = 50;
+if (num <= 49) {
+    console.log('Error');
+}
+else if (num > 100) {
+    console.log('Error. Слишком много');
+}
+else {
+    console.log('OK!');
+}
+
+(num === 50) ? console.log('Ok!') : console.log('Error');
+
+const number = 50;
+switch (number) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 50:
+        console.log('Верно');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+} 
